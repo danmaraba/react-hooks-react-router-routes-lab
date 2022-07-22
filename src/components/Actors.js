bf7dabce42bdfc9ae1cb5323e1ce67e9decce952
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-07-22 05:33:49 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,27 @@
+
 import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index)=>{
+        return(
+          <div key={index}>
+            <h2>Name :{actor.name}</h2>
+            Movies: <ul>
+              {(actor.movies).map((actormovie,index) => {
+                return <li key={index}>{actormovie}</li>
+              })}
+
+            </ul>
+      
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Actors;
